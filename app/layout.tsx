380cb75208258d02e6d74d599666650a5f137978
Luthfi Zulfan <huuman.luthfi@gmail.com>
2025-06-10 22:47:23 +0700

    - Migrated the file from github to vercel

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "next-themes"
+import { Navbar } from "@/components/layout/navbar"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Ada-EV - Rekomendasi Mobil Listrik",
+  description: "Platform rekomendasi mobil listrik",
+    generator: 'ada-ev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="id" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
